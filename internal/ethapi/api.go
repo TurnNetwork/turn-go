b1155ff5f7dd54a4420c6418b5513d37f3096153
shinnng <shinnng@outlook.com>
2023-05-11 16:22:29 +0800

    Revert "Allow for unprotected (non EIP155 signed) transactions to be submitted and executed"
    This reverts commit 6b21b8b4b203f76451871e3be09d796b0ea0f47f.

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1590,7 +1590,7 @@ func SubmitTransaction(ctx context.Context, b Backend, tx *types.Transaction) (c
 		return common.Hash{}, err
 	}
 	if tx.To() == nil {
-		signer := types.MakeSigner(b.ChainConfig(), true, true)
+		signer := types.MakeSigner(b.ChainConfig(), true)
 		from, err := types.Sender(signer, tx)
 		if err != nil {
 			return common.Hash{}, err
