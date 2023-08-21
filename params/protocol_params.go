bea98670e8cd399af20f87cb694fc1e87c3cd941
DavidRomy <934803813@qq.com>
2023-08-21 15:50:28 +0800

    code optimization

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -158,7 +158,7 @@ const (
 	StakingGas            uint64 = 6000 // Gas needed for precompiled contract: stakingContract
 	StakingTokenGas       uint64 = 6000
 	WithdrewTokenGas      uint64 = 6000
-	SettlementBubbleGas   uint64 = 6000
+	SettleBubbleGas       uint64 = 6000
 	CreateStakeGas        uint64 = 32000 // Gas needed for createStaking
 	EditCandidateGas      uint64 = 12000 // Gas needed for editCandidate
 	IncStakeGas           uint64 = 20000 // Gas needed for increaseStaking
